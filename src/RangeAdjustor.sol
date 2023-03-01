530955862accb2b7bdc243a0b5af5c0a54682890
kalinbas <kalinbas@gmail.com>
2023-03-01 13:05:29 -0600

    added fee handling, many improvements

--- src/RangeAdjustor.sol
+++ src/RangeAdjustor.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./V3Utils.sol";
 
 /// @title RangeAdjustor
-/// @notice Allows owner of RangeAdjustor contract (Revert controlled bot) to change range for configured positions
+/// @notice Allows operator of RangeAdjustor contract (Revert controlled bot) to change range for configured positions
 /// Positions need to be approved for the contract and configured with setConfig method
 contract RangeAdjustor is Ownable {
 
@@ -14,8 +14,12 @@ contract RangeAdjustor is Ownable {
     error WrongContract();
     error InvalidConfig();
     error NotSupportedFeeTier();
+    error AdjustStateError();
+    error NotConfigured();
 
+    event PositionConfigured(uint256 indexed tokenId);
     event RangeChanged(uint256 indexed oldTokenId, uint256 indexed newTokenId);
+    event OperatorChanged(address indexed oldOperator, address indexed newOperator);
 
     uint256 private constant Q64 = 2**64; 
     uint256 private constant Q96 = 2**96; 
@@ -23,6 +27,12 @@ contract RangeAdjustor is Ownable {
     V3Utils public immutable v3Utils;
     INonfungiblePositionManager immutable public nonfungiblePositionManager;
     IUniswapV3Factory public immutable factory;
+
+    // to store current token id while doing adjust()
+    uint256 private currentTokenId;
+
+    // operator
+    address public operator;
 
     struct PositionConfig {
         int24 lowerTickLimit; // if negative also in-range positions may be adjusted
@@ -32,23 +42,35 @@ contract RangeAdjustor is Ownable {
         int24 upperTickDelta; // must greater than 0 and positive multiple of tick spacing
 
         int64 maxSlippageX64; // max allowed swap slippage including fees, price impact and slippage - from current pool price (to be sure revert bot can not do silly things)
+        int64 maxGasFeeRewardX64; // max allowed token percentage to be available for covering gas cost of operator (operator chooses which one of the two tokens to receive)
     }
 
-    mapping(uint => PositionConfig) public configs;
+    // configured tokens
+    mapping(uint256 => PositionConfig) public configs;
 
-    constructor(V3Utils _v3Utils, address _swapRouter)  {
+    constructor(V3Utils _v3Utils, address _swapRouter, address _operator)  {
         v3Utils = _v3Utils;
         INonfungiblePositionManager npm = _v3Utils.nonfungiblePositionManager();
         nonfungiblePositionManager = npm;
         factory = IUniswapV3Factory(npm.factory());
+        operator = _operator;
+        emit OperatorChanged(address(0), _operator);
+    }
+
+    /**
+     * @notice Admin function to change operator address
+     */
+    function changeOperator(address _operator) onlyOwner external {
+        emit OperatorChanged(operator, _operator);
+        operator = _operator;
     }
 
     /**
      * @notice Sets config for a given NFT - must be owner
      */
-    function setConfig(uint tokenId, PositionConfig calldata config) external {
+    function setConfig(uint256 tokenId, PositionConfig calldata config) external {
 
-        if (config.lowerTickDelta < 0 || config.upperTickDelta < 0) {
+        if (config.lowerTickDelta > 0 || config.upperTickDelta <= 0) {
             revert InvalidConfig();
         }
 
@@ -57,27 +79,45 @@ contract RangeAdjustor is Ownable {
             revert Unauthorized();
         }
         configs[tokenId] = config;
+
+        emit PositionConfigured(tokenId);
     }
 
     struct AdjustParams {
-        uint tokenId;
+        uint256 tokenId;
         bool swap0To1;
-        uint256 amountIn;
+        uint256 amountIn; // if this is set to 0 no swap happens
         bytes swapData;
-        uint deadline;
+        uint256 deadline; // for uniswap operations
+        bool takeFeeFrom0;
+        uint256 feeAmount;
     }
 
     /**
      * @notice Adjust token (must be in correct state)
-     * Swap needs to be done with max price difference from current pool price
+     * Can be called only from configured operator account
+     * Swap needs to be done with max price difference from current pool price - otherwise reverts
      */
-    function adjust(AdjustParams calldata params) onlyOwner external returns (uint256 newTokenId) 
-    {
+    function adjust(AdjustParams calldata params) external {
+        // if already in an adjustment - do not allow reentrancy
+        if (currentTokenId != 0) {
+            revert AdjustStateError();
+        }
+
+        if (msg.sender != operator) {
+            revert Unauthorized();
+        }
+
         PositionConfig storage config = configs[params.tokenId];
+        if (config.upperTickDelta == 0) {
+            revert NotConfigured();
+        }
+
+        // check if in valid range for move range
         (,,address token0,address token1, uint24 fee, int24 tickLower, int24 tickUpper, uint128 liquidity, , , , ) = nonfungiblePositionManager.positions(params.tokenId);
+
         IUniswapV3Pool pool = _getPool(token0, token1, fee);
         (uint160 sqrtPriceX96, int24 currentTick,,,,,) = pool.slot0();
-
         if (currentTick < tickLower - config.lowerTickLimit || currentTick >= tickUpper + config.upperTickLimit) {
 
             // calculate with current pool price 
@@ -86,6 +126,10 @@ contract RangeAdjustor is Ownable {
             uint24 tickSpacing = _getTickSpacing(fee);
 
             address owner = nonfungiblePositionManager.ownerOf(tokenId);
+
+            // before starting process - set context id
+            currentTokenId = params.tokenId;
+
             // change range with v3utils
             bytes memory data = abi.encode(V3Utils.Instructions(
                 V3Utils.WhatToDo.CHANGE_RANGE,
@@ -105,13 +149,16 @@ contract RangeAdjustor is Ownable {
                 0,
                 0,
                 deadline,
-                owner,
-                address(this), // TODO needs to be able to redirect the new NFT - needs change in V3Utils
-                true,
+                address(this), // receive leftover tokens to grab fees - and then return rest to owner
+                address(this), // recieve the new NFT to register config and then resend
+                false,
                 "",
-                abi.encode(owner, params.tokenId) // sent along with new NFT to onERC721Received callback
+                abi.encode(owner, params.takeFeeFrom0, params.feeAmount, sqrtPriceX96) // pass needed parameters to onERC721Received
             ));
-            nonfungiblePositionManager.safeTransferFrom(owner, address(v3Utils), tokenId, data);
+            nonfungiblePositionManager.safeTransferFrom(owner, address(v3Utils), params.tokenId, data);
+
+            // after everything is finished - reset this to 0
+            currentTokenId = 0;
         }
     }
 
@@ -128,14 +175,70 @@ contract RangeAdjustor is Ownable {
             revert WrongContract();
         }
 
-        (address owner, uint256 previousTokenId) = abi.decode(data, (address, uint256));
+        // get previous token id from contract state
+        uint256 previousTokenId = currentTokenId;
+        if (previousTokenId == 0) {
+            revert AdjustStateError();
+        }
 
+        // get context variables forwarded through data
+        (address owner, bool takeFeeFrom0, uint256 feeAmount, uint160 sqrtPriceX96) = abi.decode(data, (address, bool, uint256, uint160));
+       
         // copy token config for new token
         configs[tokenId] = configs[previousTokenId];
 
-        nonfungiblePositionManager.safeTransferFrom(address(this), owner, tokenId, data);
+        // delete config for old position
+        delete configs[previousTokenId];
+
+        emit PositionConfigured(tokenId);
+
+        // forwards to real owner - forwarding data
+        nonfungiblePositionManager.safeTransferFrom(address(this), owner, tokenId, "");
+
+        // balances (leftover tokens from change range operation)
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+
+        // check amount of leftover tokens - grab fee - return rest
+        (,,address token0,address token1,, int24 tickLower, int24 tickUpper, uint128 liquidity, , , , ) = nonfungiblePositionManager.positions(tokenId);
+
+        // position value (position has no fees it has been just created - so only liquidity)
+        (uint256 amount0, uint256 amount1) = LiquidityAmounts.getAmountsForLiquidity(sqrtPriceX96, TickMath.getSqrtRatioAtTick(tickLower), TickMath.getSqrtRatioAtTick(tickUpper), liquidity);
+
+        // max fee in feeToken
+        uint256 priceX96 = FullMath.mulDiv(sqrtPriceX96, sqrtPriceX96, Q96);
+        uint256 totalFeeTokenAmount = takeFeeFrom0 ? balance0 + amount0 + FullMath.mulDiv(balance1 + amount1, priceX96, Q96) : balance1 + amount1 + FullMath.mulDiv(balance0 + amount0, Q96, priceX96);
+
+        // calculate max permited fee amount for this position
+        uint256 maxFeeAmount = FullMath.mulDiv(totalFeeTokenAmount, configs[previousTokenId].maxFeeAmount, Q64);
+
+        // calculate fee amount which can be sent.. it can be less.. so it is the operators duty to do correct swap
+        uint256 effectiveFeeAmount = (feeAmount > (takeFeeFrom0 ? balance0 : balance1) ? (takeFeeFrom0 ? balance0 : balance1) : feeAmount);
+        if (effectiveFeeAmount > maxFeeAmount) {
+            effectiveFeeAmount = maxFeeAmount;
+        }
+
+        // send fee to operator
+        if (effectiveFeeAmount > 0) {
+            IERC20(takeFeeFrom0 ? token0 : token1).transfer(operator, effectiveFeeAmount);
+            if (takeFeeFrom0) {
+                balance0 -= effectiveFeeAmount;
+            } else {
+                balance1 -= effectiveFeeAmount;
+            }
+        }
+
+        // return leftover tokens to owner
+        if (balance0 > 0) {
+            IERC20(token0).transfer(owner, balance0);
+        }
+        if (balance1 > 0) {
+            IERC20(token1).transfer(owner, balance1);
+        }
 
         emit RangeChanged(previousTokenId, tokenId);
+
+        return IERC721Receiver.onERC721Received.selector;
     }
 
     // helper method to get pool for token
@@ -144,7 +247,7 @@ contract RangeAdjustor is Ownable {
     }
 
     // helper method to get tickspacing for fee tier
-    function _getTickSpacing(uint24 fee) internal pure returns (uint) {
+    function _getTickSpacing(uint24 fee) internal pure returns (uint256) {
         if (fee == 10000) {
             return 200;
         } else if (fee == 3000) {
