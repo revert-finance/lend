ec1081f3730349e6ca0dcb0f7a35386cc70699d6
kalinbas <kalinbas@gmail.com>
2023-05-17 11:40:27 -0600

    added oracle modes

--- src/Oracle.sol
+++ src/Oracle.sol
@@ -26,10 +26,17 @@ interface AggregatorV3Interface {
 }
 contract Oracle is PriceOracle, Ownable {
 
-    uint256 constant Q96 = 2**96;
+    uint256 consoletant Q96 = 2**96;
 
     error NoFeedConfigured();
     error InvalidPool();
+
+    enum Mode {
+        CHAINLINK_TWAP_VERIFY, // using chainlink for price and TWAP to verify
+        TWAP_CHAINLINK_VERIFY, // using TWAP for price and chainlink to verify
+        CHAINLINK, // using only chainlink directly
+        TWAP // using TWAP directly
+    }
 
     struct FeedConfig {
         AggregatorV3Interface feed; // chainlink feed
@@ -40,6 +47,8 @@ contract Oracle is PriceOracle, Ownable {
         bool isToken0;
         uint8 otherDecimals;
         uint32 twapSeconds;
+        Mode mode;
+        uint16 maxDifference; // max price difference x10000
     }
 
     // ctoken => config mapping
@@ -48,7 +57,8 @@ contract Oracle is PriceOracle, Ownable {
     constructor() {
     }
 
-    function setTokenFeed(address cToken, AggregatorV3Interface feed, uint32 maxFeedAge, IUniswapV3Pool pool, uint32 twapSeconds) external onlyOwner {
+    // may be called again to update configuration
+    function setTokenFeed(address cToken, AggregatorV3Interface feed, uint32 maxFeedAge, IUniswapV3Pool pool, uint32 twapSeconds, Mode mode, uint16 maxDifference) external onlyOwner {
         uint8 feedDecimals = feed.decimals();
         address underlying = CErc20Interface(address(cToken)).underlying();
         uint8 tokenDecimals = IERC20Metadata(underlying).decimals();
@@ -62,12 +72,17 @@ contract Oracle is PriceOracle, Ownable {
             otherToken = pool.token1();
         }
         uint8 otherDecimals = IERC20Metadata(otherToken).decimals();
-        feedConfigs[cToken] = FeedConfig(feed, maxFeedAge, feedDecimals, tokenDecimals, pool, isToken0, otherDecimals, twapSeconds);
+        feedConfigs[cToken] = FeedConfig(feed, maxFeedAge, feedDecimals, tokenDecimals, pool, isToken0, otherDecimals, twapSeconds, mode, maxDifference);
+    }
+
+    function setOracleMode(address cToken, Mode mode) external onlyOwner {
+        feedConfigs[cToken].mode = mode;
     }
 
     function _getReferencePoolPrice(IUniswapV3Pool pool, uint32 twapSeconds, bool isToken0, uint8 tokenDecimals, uint8 otherDecimals, uint8 feedDecimals) internal view returns (uint256) {
 
         uint160 sqrtPriceX96;
+        // if twap seconds set to 0 just use pool price
         if (twapSeconds == 0) {
             (sqrtPriceX96,,,,,,) = pool.slot0();
         } else {
@@ -89,29 +104,54 @@ contract Oracle is PriceOracle, Ownable {
 
     function getUnderlyingPrice(CToken cToken) override external view returns (uint256 result) {
         FeedConfig storage feedConfig = feedConfigs[address(cToken)];
-        if (address(feedConfig.feed) == address(0)) {
-            revert NoFeedConfigured();
+
+        uint256 price;
+        uint256 verifyPrice;
+
+        if (feedConfig.mode == Mode.CHAINLINK_TWAP_VERIFY || feedConfig.mode == Mode.TWAP_CHAINLINK_VERIFY || feedConfig.mode == Mode.CHAINLINK) {
+            if (address(feedConfig.feed) == address(0)) {
+                revert NoFeedConfigured();
+            }
+
+            // if stale data - return 0 - handled as error in compound
+            (, int256 answer, , uint256 updatedAt, ) = feedConfig.feed.latestRoundData();
+            if (updatedAt + feedConfig.maxFeedAge < block.timestamp) {
+                return 0;
+            }
+            // if invalid data - return 0 - handled as error in compound
+            if (answer <= 0) {
+                return 0;
+            }
+
+            if (feedConfig.mode == Mode.TWAP_CHAINLINK_VERIFY) {
+                verifyPrice = uint256(answer);
+            } else {
+                price = uint256(answer);
+            }
         }
 
-        // if stale data - return 0 - handled as error in compound
-        (, int256 answer, , uint256 updatedAt, ) = feedConfig.feed.latestRoundData();
-        if (updatedAt + feedConfig.maxFeedAge < block.timestamp) {
-            return 0;
+        if (feedConfig.mode == Mode.CHAINLINK_TWAP_VERIFY || feedConfig.mode == Mode.TWAP_CHAINLINK_VERIFY || feedConfig.mode == Mode.TWAP) {
+             // get reference pool price
+            uint256 poolTWAPPrice = _getReferencePoolPrice(feedConfig.pool, feedConfig.twapSeconds, feedConfig.isToken0, feedConfig.tokenDecimals, feedConfig.otherDecimals, feedConfig.feedDecimals);
+
+            if (feedConfig.mode == Mode.CHAINLINK_TWAP_VERIFY) {
+                verifyPrice = poolTWAPPrice;
+            } else {
+                price = poolTWAPPrice;
+            }
         }
-        // if invalid data - return 0 - handled as error in compound
-        if (answer < 0) {
-            return 0;
+        
+        if (feedConfig.mode == Mode.CHAINLINK_TWAP_VERIFY || feedConfig.mode == Mode.TWAP_CHAINLINK_VERIFY) {
+            uint256 difference = price > verifyPrice ? FullMath.mulDiv(price - verifyPrice, 10000, price) : FullMath.mulDiv(verifyPrice - price, 10000, verifyPrice);
+
+            // if too big difference - return 0 - handled as error in compound
+            if (difference >= feedConfig.maxDifference) {
+                return 0;
+            }
         }
 
-        // get reference pool price
-        uint256 poolPrice = _getReferencePoolPrice(feedConfig.pool, feedConfig.twapSeconds, feedConfig.isToken0, feedConfig.tokenDecimals, feedConfig.otherDecimals, feedConfig.feedDecimals);
-
-        // if price is off - return 0 - handled as error in compound
-        // TODO
-
-        // TODO handle different modes
 
         // convert to compound expected format
-        result = (10 ** (36 - feedConfig.feedDecimals - feedConfig.tokenDecimals)) * uint256(answer);
+        result = (10 ** (36 - feedConfig.feedDecimals - feedConfig.tokenDecimals)) * price;
     }
 }
