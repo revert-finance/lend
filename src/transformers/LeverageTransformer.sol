9fb334e2dd366742c8958b6ede41faa6824cceb5
kalinbas <kalinbas@gmail.com>
2023-11-07 10:31:45 -0600

    fix leftover token handling in leverage

--- src/transformers/LeverageTransformer.sol
+++ src/transformers/LeverageTransformer.sol
@@ -51,6 +51,9 @@ contract LeverageTransformer {
         uint256 amountOut1Min;
         bytes swapData1; // encoded data from 0x api call (address,bytes) - allowanceTarget,data
 
+        // recipient for leftover tokens
+        address recipient;
+
         // for all uniswap deadlineable functions
         uint256 deadline;
     }
@@ -58,45 +61,45 @@ contract LeverageTransformer {
     // method called from transform() method in Vault
     function leverageUp(LeverageUpParams calldata params) external {
 
-        uint borrowAmount = params.borrowAmount;
+        uint amount = params.borrowAmount;
 
         address token = IVault(msg.sender).lendToken();
-        IVault(msg.sender).borrow(params.tokenId, borrowAmount);
+        IVault(msg.sender).borrow(params.tokenId, amount);
 
         (,,address token0, address token1,,,,,,,,) = nonfungiblePositionManager.positions(params.tokenId);
 
-        uint amount0 = token == token0 ? borrowAmount : 0;
-        uint amount1 = token == token1 ? borrowAmount : 0;
+        uint amount0 = token == token0 ? amount : 0;
+        uint amount1 = token == token1 ? amount : 0;
 
         if (params.amountIn0 > 0) {
             (uint amountIn, uint amountOut) = _swap(IERC20(token), IERC20(token0), params.amountIn0, params.amountOut0Min, params.swapData0);
             if (token == token1) {
                 amount1 -= amountIn;
-                borrowAmount -= amountIn;
             }
+            amount -= amountIn;
             amount0 += amountOut;
         }
         if (params.amountIn1 > 0) {
             (uint amountIn, uint amountOut) = _swap(IERC20(token), IERC20(token1), params.amountIn1, params.amountOut1Min, params.swapData1);
             if (token == token0) {
                 amount0 -= amountIn;
-                borrowAmount -= amountIn;
             }
+            amount -= amountIn;
             amount1 += amountOut;
         }
 
         INonfungiblePositionManager.IncreaseLiquidityParams memory increaseLiquidityParams = INonfungiblePositionManager.IncreaseLiquidityParams(params.tokenId, amount0, amount1, params.amountAddMin0, params.amountAddMin1, params.deadline);
         (, uint added0, uint added1) = nonfungiblePositionManager.increaseLiquidity(increaseLiquidityParams);
 
-        address owner = IVault(msg.sender).ownerOf(params.tokenId);
+        // send leftover tokens
         if (amount0 > added0) {
-            IERC20(token0).transfer(owner, amount0 - added0);
+            IERC20(token0).transfer(params.recipient, amount0 - added0);
         }
         if (amount1 > added1) {
-            IERC20(token1).transfer(owner, amount1 - added1);
+            IERC20(token1).transfer(params.recipient, amount1 - added1);
         }
-        if (token != token0 && token != token1 && borrowAmount > 0) {
-            IERC20(token).transfer(owner, borrowAmount);
+        if (token != token0 && token != token1 && amount > 0) {
+            IERC20(token).transfer(params.recipient, amount);
         }
     }
 
@@ -125,6 +128,9 @@ contract LeverageTransformer {
         // if token1 needs to be swapped to targetToken - set values
         uint256 amountOut1Min;
         bytes swapData1; // encoded data from 0x api call (address,bytes) - allowanceTarget,data
+
+        // recipient for leftover tokens
+        address recipient;
 
         // for all uniswap deadlineable functions
         uint256 deadline;
@@ -162,15 +168,15 @@ contract LeverageTransformer {
         uint repayed = IVault(msg.sender).repay(params.tokenId, amount, false);
         amount -= repayed;
 
-        address owner = IVault(msg.sender).ownerOf(params.tokenId);
-        if (amount0 > 0) {
-            IERC20(token0).transfer(owner, amount0);
+        // send leftover tokens
+        if (amount0 > 0 && token != token0) {
+            IERC20(token0).transfer(params.recipient, amount0);
         }
-        if (amount1 > 0) {
-            IERC20(token1).transfer(owner, amount1);
+        if (amount1 > 0 && token != token1) {
+            IERC20(token1).transfer(params.recipient, amount1);
         }
-        if (token != token0 && token != token1) {
-            IERC20(token).transfer(owner, amount);
+        if (amount > 0) {
+            IERC20(token).transfer(params.recipient, amount);
         }    
     }
 
@@ -209,6 +215,4 @@ contract LeverageTransformer {
             emit Swap(address(tokenIn), address(tokenOut), amountInDelta, amountOutDelta);
         }
     }
-
-
 }
