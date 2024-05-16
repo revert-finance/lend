ab8a03114a88429d1e1ccd73dd5f2fba71f1206e
kalinbas <kalinbas@gmail.com>
2024-05-15 18:00:33 -0600

    chainlink feed combinator

---
+++ src/utils/ChainlinkFeedCombinator.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import "../../lib/AggregatorV3Interface.sol";
+
+/// @title Helper contract which allows to combine 2 chainlink feeds into 1 like wstETH/ETH and ETH/USD
+contract ChainlinkFeedCombinator is AggregatorV3Interface {
+  
+    uint256 immutable firstDecimalsDivisor;
+    uint8 immutable secondDecimals;
+    AggregatorV3Interface immutable firstFeed;
+    AggregatorV3Interface immutable secondFeed;
+
+    constructor(AggregatorV3Interface first, AggregatorV3Interface second) {
+        firstDecimalsDivisor = 10 ** first.decimals();
+        secondDecimals = second.decimals();
+
+        firstFeed = first;
+        secondFeed = second;
+    }
+
+    function latestRoundData() external override view returns (
+        uint80 roundId,
+        int256 answer,
+        uint256 startedAt,
+        uint256 updatedAt,
+        uint80 answeredInRound
+    ) {
+        (uint80 firstRoundId, int256 firstAnswer,uint256 firstStartedAt, uint256 firstUpdatedAt, uint80 firstAnsweredInRound) = firstFeed.latestRoundData();
+        int256 secondAnswer;
+        (roundId, secondAnswer, startedAt, updatedAt, answeredInRound) = secondFeed.latestRoundData();
+
+        // take oldest values
+        if (updatedAt > firstUpdatedAt) {
+            roundId = firstRoundId;
+            firstStartedAt = firstStartedAt;
+            updatedAt = firstUpdatedAt;
+            answeredInRound = firstAnsweredInRound;
+        }
+
+        // only do calculation with valid values - otherwise returns 0
+        if (firstAnswer > 0 && secondAnswer > 0) {
+            answer = int256(uint256(firstAnswer) * uint256(secondAnswer) / firstDecimalsDivisor);
+        }
+    }
+
+    function decimals() external override view returns (uint8) {
+        return secondDecimals;
+    }
+}
